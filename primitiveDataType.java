274449c7d2738a9d65b8c392f74691012d43f031
Chitranshu2512 <118387802+Chitranshu2512@users.noreply.github.com>
2023-06-25 00:18:55 +0530

    Add files via upload

---
+++ primitiveDataType.java
@@ -0,0 +1,16 @@
+public class primitiveDataType {
+    public static void main(String[] args) {
+        int x=5;
+        int y=7;
+        System.out.println("x is :"+x);
+        System.out.println("y is :"+y);
+
+        byte b=10;
+        System.out.println("B is :"+b);
+
+        byte b1= (byte)130;
+        System.out.println("B is :"+b1);
+
+
+    }
+}
